674bd8c8a3bcaed738760f06ffa9c626166d12af
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2010-06-25 21:28:48 +0200

    prepared feature text for release

--- trac_captcha/release.py
+++ trac_captcha/release.py
@@ -4,30 +4,31 @@ name = 'TracCaptcha'
 version = '0.1'
 description = 'pluggable captcha infrastructure for trac with reCAPTCHA included'
 long_description = '''
-TracCaptcha is a set of trac plugins to embed captchas in addition to Trac's 
-regular permission checks so that spammers are kept out.
-
-Generic Infrastructure: TracCaptcha takes care about embedding a
-given captcha in the appropriate places which means that building a different 
-captcha plugin is easy - you just have to build the captcha itself while this
-code will take care of the trac integration.
-
-Flexible Permissions: You can configure that the captcha is not displayed for 
-certain users or groups (e.g. 'all authenticated users') just by using Trac's
-permission system.
+TracCaptcha is a trac plugin to embed a captcha in the ticket page in addition 
+to Trac's regular permission checks so that spammers are kept out.
 
 Batteries included: The popular reCAPTCHA system is supported out of the box.
 Technically it's a plugin - if you don't like it you're free to use any other 
 plugin while still leverage the benefits from the general captcha 
 infrastructure.
 
+Does not annoy users: After the user entered the captcha once, he does not have
+to solve the captcha again for the same ticket when he just clicks 'preview'.
+Also you can configure exempt certain users or groups (e.g. 'all authenticated 
+users') from the captchas just by using Trac's permission system.
+
+Generic Infrastructure: TracCaptcha takes care about embedding a
+given captcha in the appropriate places which means that building a different 
+captcha plugin is easy - you just have to build the captcha itself while this
+code will take care of the trac integration.
+
 
 Changelog
 ******************************
 
-0.1
+0.1 (25.06.2010)
 ==================
- - initial release: you can protect ticket pages with reCAPTCHA
+ - initial release
 '''
 author = 'Felix Schwarz'
 email = 'felix.schwarz@oss.schwarz.eu'
